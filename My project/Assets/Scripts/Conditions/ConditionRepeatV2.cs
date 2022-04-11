559ca4e9cf7da88538aadab3247a069362864065
pipepipe3 <felipe2zamora6@gmail.com>
2022-04-11 18:14:00 -0400

    pipe top down

---
+++ My project/Assets/Scripts/Conditions/ConditionRepeatV2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ConditionRepeatv2 : MonoBehaviour
+{
+	public float initialDelay = 0f;
+	public float frequency = 1f;
+	float actualTime = 0;
+	public bool useInitialDelay = false;
+
+	public UnityEvent action;
+
+	private void Start()
+	{
+		actualTime = 0;
+        if (useInitialDelay)
+        {
+			actualTime = frequency - initialDelay;
+		}
+	}
+
+
+	private void Update()
+	{
+		actualTime += Time.deltaTime;
+		if (actualTime >= frequency)
+		{
+			if (action != null) action.Invoke();
+			actualTime = 0;
+		}
+	}
+}
